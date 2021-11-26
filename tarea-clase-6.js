944e68653327168b968fe3ce3c2bddd9be90e62d
marcop96 <55959162+marcop96@users.noreply.github.com>
2021-11-26 18:06:05 +0100

    estructura
    se crea la estructura del codigo para comenzar a plantear el ejercicio

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -6,14 +6,20 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
-
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
 Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
-console.log("d")
+---------------------------
 
-prompt('asd')
+preguntar cantidad de integrantes en un input
+         - al presionar un boton se crean inputs y labels en base a la cantidad de integrantes
+        contenido de label (integrante familiar #X) - inputs placeholder (ingresar edad del integrante X)
+        botón calcular 
+          - muestra tres campos pre-existentes (ocultos)
+          calcula menor mayor promedio en esos 3 campos
+         
+*/
+
