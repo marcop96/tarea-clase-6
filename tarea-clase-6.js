159a42afef3a4bc9ea490ed97b815bcb94c8b224
marcop96 <55959162+marcop96@users.noreply.github.com>
2022-05-25 18:57:23 +0200

    create onclick function

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -7,6 +7,31 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 */
 
 /*
+*crear input que pregunta cantidad de integrantes + boton para confirmar 
+*crear div oculto para las respuestas
+
+!boton asigna valor a la cantidad de integrantes
+!funciones mayor menor promedio
+
+
+*/
+
+let $botonConfirmar = document.querySelector(".confirmar");
+
+$botonConfirmar.onclick = function () {
+  let $cantidadIntegrantes = document.querySelector(
+    ".cantidad-integrantes"
+  ).value;
+  let numeroIntegrantes = 0;
+
+  for (let i = 0; i < $cantidadIntegrantes; i++) {
+    numeroIntegrantes++;
+
+    console.log(numeroIntegrantes);
+  }
+};
+
+/*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
 Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual, menor salario anual, salario anual promedio y salario mensual promedio.
@@ -14,15 +39,5 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor sala
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 ---------------------------
 
-
-
-
-crear input que pregunta cantidad de integrantes + boton para confirmar
-
- 
-
-
-
-crear div oculto para las respuestas
 
 */
